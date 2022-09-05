a620c05e07b15810639f437b01a6bb92cdede9af
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-05 15:52:44 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,15 @@
 def call(Map config = [:]){
  sh "echo ${config.name} How are you, today is ${config.dayOfWeek}" 
 }
+
+def sayGoodMorning(){
+ sh "echo  Calling from sayGoodMorning method"
+}
+
+def sayGoodEvening(){
+ sh "echo  Calling from sayGoodEvening method"
+}
+
+def sayGoodNight(){
+ sh "echo  Calling from sayGoodNight method"
+}
