4b083c84f16a6c99d4096b8fc2b27be98fce831e
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-07 16:35:24 +0530

    Create createMultibranchPipelines.groovy

---
+++ vars/createMultibranchPipelines.groovy
@@ -0,0 +1,13 @@
+def generateFolders(List<Path> jenkinsfilePaths, Path rootFolder) {
+    jenkinsfilePaths
+            .collect { ancestorFoldersPath(rootFolder.resolve(it).parent) }
+            .flatten()
+    // Remove duplicates in case some Jenkinsfiles share ancestors for optimization.
+            .unique()
+            .each {
+                // Effectively provision the folder.
+                folder(it.toString())
+            }
+}
+
+generateFolders(jenkinsfilePaths,rootFolder)
