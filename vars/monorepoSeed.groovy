4c8e58223797b30df4d66d595b524a6bc7b95552
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-07 15:19:26 +0530

    Update monorepoSeed.groovy

--- vars/monorepoSeed.groovy
+++ vars/monorepoSeed.groovy
@@ -3,11 +3,18 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    
+    String repositoryName = env.JOB_NAME.split('/')[1]
+    String rootFolderPath = "Generated/$repositoryName"
 
     pipeline{
         agent any
             stages{
                 stage('folder'){
+                    echo "--------------------------"
+                    echo repositoryName.toString()
+                    echo rootFolderPath.toString()
+                    echo "--------------------------"
                     steps{
                         script{                       
                             jobDsl scriptText: "folder('prasadFolder')",
