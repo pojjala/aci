3b6eef8f79c84c4135989f1487c9eda2a67df987
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-11 20:11:25 +0530

    Update createFolder.groovy

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -17,7 +17,7 @@ def call(body) {
                     steps{
                         checkout scm
                         script{   
-                          jobDsl(scriptText: "folder('folderName')")  
+                            dsl{(scriptText: "folder('folderName')") }
 //                           jobDsl(scriptText: libraryResource('aci/vars/myFoldertest.groovy'),removedJobAction: 'IGNORE')   
                         }
                     }
