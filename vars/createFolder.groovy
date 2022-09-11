8808d1dfeb8d5a7966432702b8178b396e7468f2
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-11 23:17:45 +0530

    Update createFolder.groovy

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -7,27 +7,17 @@ def call(body) {
     pipeline{
         agent any
         environment{
-//             BITBUCKET_PROJECT_URL = "https://github.com/pojjala/payments.git"
-//             REPOSITORY_ROOT = "payments"
-//             INCLUDE_PATTERN = "develope feature/app1"
             PROJECT_NAME = "ACIWorldWide"
             }
         stages{
             stage('Creation of folders'){
                     steps{
-//                         checkout scm
+                        checkout scm
                         script{   
-//                         jobDsl {
-//                                 folder('project-a') {
-//                                     displayName('Project A')
-//                                     description('Folder for project A')
-//                                 }                        
-//                             }
-                            jobDsl(scriptText: "folder('project-ovln')")
+                             jobDsl(scriptText: "folder('project-ovln')")
 //                           jobDsl(scriptText: libraryResource('aci/vars/myFoldertest.groovy'),removedJobAction: 'IGNORE')   
-//                         }
-                    }
-                }
-      }
-    }
+                           }
+                  }
+             }
+     }
 }
