828069aa461f22c9767471e406ac45b50b81b086
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-14 16:58:48 +0530

    Update createFolder.groovy

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -1,27 +1,42 @@
 def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    
-    pipeline{
-        agent any
-        environment{
-            PROJECT_NAME = "ACIWorldWide"
+    def project = 'payments'
+//     https://github.com/pojjala/payments.git
+    def branchApi = new URL("https://github.com/pojjala/${project}/branches")
+    def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+    branches.each {
+        def branchName = it.name
+        def jobName = "${project}-${branchName}".replaceAll('/','-')
+        job(jobName) {
+            scm {
+                git("git://github.com/${project}.git", branchName)
             }
-        stages{
-            stage('Creation of folders'){
-                    steps{
-                        checkout scm
-                        dsl{ 
-                            folder('project-ovln')
-                           }
+            steps {
+                maven("test -Dproject.name=${project}/${branchName}")
+            }
+        }
+    }
+    
+    
+//     def config = [:]
+//     body.resolveStrategy = Closure.DELEGATE_FIRST
+//     body.delegate = config
+//     body()
+    
+//     pipeline{
+//         agent any
+//         environment{
+//             PROJECT_NAME = "ACIWorldWide"
+//             }
+//         stages{
+//             stage('Creation of folders'){
+//                     steps{
+//                         checkout scm
 //                         script{   
-//                              jobDsl(scriptText: "folder('project-ovln')")
-// //                           jobDsl(scriptText: libraryResource('aci/vars/myFoldertest.groovy'),removedJobAction: 'IGNORE')   
+// //                              jobDsl(scriptText: "folder('project-ovln')")
+//                           jobDsl(scriptText: libraryResource('aci/vars/myFoldertest.groovy'),removedJobAction: 'IGNORE')   
 //                            }
-                        }
-             }
-        }  
-     }
+//                         }
+//              }
+//         }  
+//      }
 }
