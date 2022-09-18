0296d43ec373fb23f18594050b252fee95471475
Prasad Ojjala <prasad.ojjala@gmail.com>
2022-09-18 18:15:18 +0530

    Update createFolder.groovy

--- vars/createFolder.groovy
+++ vars/createFolder.groovy
@@ -16,7 +16,7 @@ def call(body) {
         stages{
             stage('Creation of folders'){
                     steps{
-                        PROJECT_NAME = "ACIWorldWide"
+                        sh '''export PROJECT_NAME = "ACIWorldWide" '''
                         checkout scm
                         script{   
                          echo "Prepare workspace"
